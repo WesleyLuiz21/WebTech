10a7c7e5bf8cc00ee173feb262e9cc3f19000808
Wesley Souza <wesley_luiz2001@hotmail.com>
2025-03-15 16:20:18 +0000

    pwp

---
+++ js.js
@@ -0,0 +1,12 @@
+
+
+let menu = document.getElementById("menu");
+let h3 = document.getElementById("title");+
+
+function nope(){
+  menu.style.display = "none";
+}
+
+function yes(){
+    h3.innerHTML = "First Lesson - Don't admit that"
+}
